688113c6c38fce9a84bb371e3275e3671d106eda
lolluslollus <lolluslollus@users.noreply.github.com>
2021-09-09 12:58:25 +0200

    Update settings.lua

--- res/scripts/lollo_tweak_assets/settings.lua
+++ res/scripts/lollo_tweak_assets/settings.lua
@@ -1,7 +1,53 @@
 return {
-    isExtendedLog = false,
+    isExtendedLog = true,
     modIds = {
         '2262626670', -- different houses/buildings
+        '2591217865', -- Thomaskirche Leipzig
+        '2586667592', -- sports and leisure pack
+        '2584111996', -- westerkerk
+        '2577826043', -- bella italia
+        '2572849521', -- berliner dom
+        '2567960375', -- tower of london
+        '2563577043', -- sacré coeur
         '2560352422', -- European Buildings pack 1
+        '2556365982', -- church of the saviour on blood
+        '2551946896', -- himeji castle
+        '2546367263', -- medieval buildings
+        '2540389399', -- neuschwanstein
+        '2525438156', -- rijksmuseum
+        '2519607743', -- amsterdam houses
+        '2514014305', -- saint patrick NY
+        '2505541398', -- koelner dom
+        '2497936873', -- colosseo
+        '2487275848', -- siegessaeule
+        '2479174195', -- westminster palace
+        '2472627766', -- brandenburger tor
+        '2465189075', -- arc de triomphe
+        '2460048665', -- notre dame
+        '2389430377', -- Residential building series 1Lg-600
+        '2203094600', -- Residential building II-08
+        '2129230721', -- House series P-30
+        '2124887660', -- Kindergarten, project VI-52
+        '2125770168', -- House series II-67-Tishinskaya tower
+        '2125833090', -- House series I-700
+        '2021752763', -- Tool shop
+        '2020020521', -- Series 1-335 house
+        '2019578161', -- project_1X-01-15
+        '2019053493', -- Model department stores. Series 2-07
+        '2018605575', -- series 1-201 houses.
+        '2018513512', -- House_type10
+        '2017957265', -- House_type1
+        '2018014162', -- House_type2
+        '2018044864', -- House_type3
+        '2018165107', -- House_type5
+        '2018330251', -- House_type9
+        '2018304981', -- House_type8
+        '2018194387', -- House_type6
+        '1982926033', -- Post EC
+        '1939679675', -- House_series_II-18
+        '1939688963', -- House_series_II-68
+        '2510205291', -- UK Tower Block Variable Height
+        '2510205703', -- UK Tower Block 2 Variable Height
+        -- '2188171677', -- urban assets - LOLLO TODO fix these, they are purely decorative
     }
 }
